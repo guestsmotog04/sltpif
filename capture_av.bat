6dd93c25afec7279e1e65ae82a539a7d3934fa0c
guestsmotog04 <guestsmotog04@gmail.com>
2026-05-06 19:02:26 +0200

    Add files via upload

---
+++ capture_av.bat
@@ -0,0 +1,27 @@
+@echo off
+title Android AV Capture
+
+echo Removing old capture file...
+del output.mp4 2>nul
+
+echo Starting scrcpy recording to scrcpy-pipe.mkv...
+start "" scrcpy --no-window --record scrcpy-pipe.mkv --record-format=mkv
+
+echo Waiting for scrcpy to finish...
+
+:wait_scrcpy
+tasklist | find /i "scrcpy.exe" >nul
+if %errorlevel%==0 (
+    timeout /t 1 >nul
+    goto wait_scrcpy
+)
+
+echo scrcpy has exited. Starting FFmpeg remux...
+ffmpeg -i scrcpy-pipe.mkv -c:v copy -c:a aac output.mp4
+move .\output.mp4 ..\finished_videos
+
+echo Deleting old capture file from scrcpy...
+del scrcpy-pipe.mkv 2>nul
+
+echo Done!
+pause
